5fe0f991ff103691d54d3938e9ede572c73a4c81
Md-Mofakhkharul-Islam-Maruf <mofakhkharulislammaruf@gmail.com>
2025-07-21 21:18:39 +0600

    made utils functions

---
+++ utils.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+
+// ------------- generateAccessToken ------------------
+const generateAccessToken = (payload) => {
+
+  // Sign the token
+  const token = jwt.sign(payload, 'secret', {
+    expiresIn: '30d',
+  });
+
+  return token;
+};
+
+module.exports = generateAccessToken;
+
+
+// ---------------- verifyToken --------------------
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ success: false, message: 'Unauthorized access - no token provided' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, 'secret');
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(403).json({ success: false, message: 'Forbidden - invalid token' });
+  }
+};
+
+module.exports = verifyToken;
